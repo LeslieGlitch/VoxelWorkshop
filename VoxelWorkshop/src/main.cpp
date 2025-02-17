6e797d4fa7823464e6ea501282daecb5b634ee08
Leslie Glitch <wollitz2.18@gmail.com>
2025-02-16 19:13:01 -0500

    Add project files.

---
+++ VoxelWorkshop/src/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// SETTINGS
+const int VIEWPORT_SIZE[] = { 800, 600 };
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main() {
+
+	// initialize GLFW
+	glfwInit();
+
+	// openGL ver. 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// use core profile
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// create window
+	GLFWwindow* window = glfwCreateWindow(VIEWPORT_SIZE[0], VIEWPORT_SIZE[1], "VoxelWorkshop", NULL, NULL);
+
+	// guard clause: failure to instantiate window
+	if (window == NULL) {
+		std::cout << "ERROR: Could not create window." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// focus on new window
+	glfwMakeContextCurrent(window);
+
+	// guard clause: failure to initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "ERROR: Failure to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// pass rendering window size to OpenGL
+	glViewport(0, 0, VIEWPORT_SIZE[0], VIEWPORT_SIZE[1]);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window)) {
+		// process input
+		processInput(window);
+
+		// render
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// send new frame to window
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// clear GLFW resources on close
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+	return;
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
