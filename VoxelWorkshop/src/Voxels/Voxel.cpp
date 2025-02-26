195cd0d9c4b9863919b66890175b89dd7c89deaa
Leslie Glitch <wollitz2.18@gmail.com>
2025-02-25 19:51:07 -0500

    Update Voxel class to store albedo color instead of Material

--- VoxelWorkshop/src/Voxels/Voxel.cpp
+++ VoxelWorkshop/src/Voxels/Voxel.cpp
@@ -6,10 +6,17 @@
 
 #include "Voxel.h"
 
-Voxel::Voxel(Material mat) : material(mat), active(true)
-{}
+Voxel::Voxel(int voxelColor[3]) : active(false) {
+	setColor(voxelColor);
+}
+
+Voxel::Voxel() : active(false) {
+	int albedo[3] = { 255, 255, 255 };
+	setColor(albedo);
+}
 
 Voxel::~Voxel() {
+	active = false;
 	return;
 }
 
@@ -18,6 +25,24 @@ void Voxel::isActive(bool isActive) {
 	return;
 }
 
-bool Voxel::isActive() {
+bool Voxel::isActive() const {
 	return active;
+}
+
+int* Voxel::getColor() const {
+	int albedo[3] = {color[0], color[1], color[2]};
+	return albedo;
+}
+
+void Voxel::setColor(int albedo[3]) {
+	for (int i = 0; i < 3; ++i) {
+		color[i] = albedo[i];
+	}
+}
+
+Voxel Voxel::operator=(const Voxel& right) {
+	Voxel result;
+	active = right.isActive();
+	setColor(right.getColor());
+	return result;
 }
--- VoxelWorkshop/src/Voxels/Voxel.h
+++ VoxelWorkshop/src/Voxels/Voxel.h
@@ -17,23 +17,38 @@ enum Material {
 class Voxel {
 public:
 	/// @brief Constructor for a single voxel
-	/// @param material Material for the current voxel, defaults to Vacuum
-	Voxel(Material material = Material::Vacuum);
+	/// @param voxelColor albedo color for the current voxel, defaults to White
+	Voxel(int voxelColor[3]);
+
+	/// @brief Default constructor for a single voxel
+	Voxel();
 
 	/// @brief Destructor for a single voxel
 	~Voxel();
 
 	/// @brief Returns whether the voxel has anything or not
 	/// @return True if the voxel is active, False otherwise
-	bool isActive();
+	bool isActive() const;
 
 	/// @brief Sets the active state of the voxel
 	/// @param active State to set voxel
 	void isActive(bool active);
 
+	/// @brief Gets the albedo color of the voxel
+	/// @return An array of 3 integers (0-255) in the form {r, g, b}
+	int* getColor() const;
+
+	/// @brief Sets the albedo color of the voxel
+	/// @param albedo Array of 3 integers (0-255) in the form {r, g, b}
+	void setColor(int albedo[3]);
+
+	// overloaded operators
+	Voxel operator=(Voxel const& right);
+
 private:
 	bool active;
-	Material material;
+	int color[3];
+	//Material material;
 };
 
 #endif
