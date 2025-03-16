3b5f1364f90e14b5fe5c08afb81c2fcb9623411a
Leslie Glitch <wollitz2.18@gmail.com>
2025-03-16 09:59:19 -0400

    Make Camera members private

--- VoxelWorkshop/src/Renderer/Camera.h
+++ VoxelWorkshop/src/Renderer/Camera.h
@@ -21,7 +21,7 @@
 #include "Shader.h"
 
 class Camera {
-public:
+private:
     glm::vec3 Position;
     glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
     glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
@@ -34,6 +34,7 @@ public:
     float speed = 0.1f;
     float sensitivity = 100.0f;
 
+public:
     Camera(int width, int height, glm::vec3 position);
 
     void Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform);
